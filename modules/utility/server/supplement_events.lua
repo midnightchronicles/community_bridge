00f69aa315ba87a09ff25599b29080caa678371f
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-01-23 23:34:21 -0500

    fix deprecated event method

--- modules/utility/server/supplement_events.lua
+++ modules/utility/server/supplement_events.lua
@@ -1,4 +1,4 @@
-RegisterServerEvent("community_bridge:server:alert", function(data)
+RegisterNetEvent("community_bridge:server:alert", function(data)
     local playersByJob = Framework.GetPlayersByJob(data.job)
     for _, src in pairs(playersByJob) do
         TriggerClientEvent("community_bridge:client:alert", src, data)
