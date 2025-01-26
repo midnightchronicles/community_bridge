2ee6286c7a3866b39984ce195c00f813d7d8c317
gononono64 <96976330+gononono64@users.noreply.github.com>
2025-01-26 12:07:26 -0600

    yeeted itemName
    Yeeted itemName because redundant and example given previously by newb was this:
    
    ```lua
     ...
        function(src, itemData)
     ...
    ```

--- modules/framework/qb-core/server.lua
+++ modules/framework/qb-core/server.lua
@@ -252,7 +252,7 @@ Framework.RegisterUsableItem = function(itemName, cb)
     local func = function(src, item, itemData)
         itemData = itemData or item
         itemData.metadata = itemData.metadata or itemData.info or {}
-        cb(src, itemName, itemData)
+        cb(src, itemData)
     end
     QBCore.Functions.CreateUseableItem(itemName, func)
 end
