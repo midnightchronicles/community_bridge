55d81c70398f223eae7d743fcbf20e8a1a399682
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-02-06 22:53:30 -0500

    clothing def, these are placeholder till functionality is done

---
+++ modules/clothing/_default/client.lua
@@ -0,0 +1,23 @@
+Clothing = Clothing or {}
+
+StoredOldClothing = {}
+
+Clothing.SetAppearance = function(clothingData)
+
+    return true
+end
+
+Clothing.GetAppearance = function()
+    local clothing = {}
+
+    return clothing
+end
+
+Clothing.RestoreAppearance = function()
+
+    return true
+end
+
+Clothing.ReloadSkin = function()
+
+end
---
+++ modules/clothing/_default/server.lua
@@ -0,0 +1,17 @@
+Clothing = Clothing or {}
+
+Clothing.SetAppearance = function(src, data)
+    --wip
+end
+
+Clothing.GetAppearance = function(src)
+    --wip
+end
+
+Clothing.RestoreAppearance = function(src)
+    --wip
+end
+
+Clothing.ReloadSkin = function(src)
+    --wip
+end
