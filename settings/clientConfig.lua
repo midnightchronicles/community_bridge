835406d74beea6d12bdc595770702765f8a3cb4d
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-03-03 19:09:45 -0500

    configs to new auto/manual preferences also removed unused values

--- settings/clientConfig.lua
+++ settings/clientConfig.lua
@@ -1,13 +1,13 @@
 BridgeClientConfig = {}
 BridgeClientConfig.DebugLevel        = 0            -- Debug level, 0 for none, 1 for some, 2 for all data
 BridgeClientConfig.NotifySystem      = "ox"         -- [ ox | qb | esx | mythic | pNotify | t-notify | wasabi | lab | custom ]
-BridgeClientConfig.HelpTextSystem    = "auto"
-BridgeClientConfig.ShowHelpText      = "ox"         -- [ox | jg | qb | lab | custom ]
+BridgeClientConfig.ShowHelpText      = "auto"       -- [ ox | qb | jg | okok | cd | lab | custom ]
 BridgeClientConfig.InputSystem       = "auto"       -- [ auto | ox | qb ]
 BridgeClientConfig.MenuSystem        = "auto"       -- [ auto | ox | qb ]
-BridgeClientConfig.ProgressBarSystem = "auto"
+BridgeClientConfig.ProgressBarSystem = "auto"       -- [ auto | ox | qb ]
 BridgeClientConfig.ZoneSystem        = "ox"         -- [ ox | poly ]
-BridgeClientConfig.ProgressBar       = "ox"         -- [ ox | qb ]
-BridgeClientConfig.VehicleKey        = "auto"        -- Change this to your prefrence or leave it as auto
+BridgeClientConfig.VehicleKey        = "auto"       -- [ auto | qb-vehiclekeys | MrNewbVehicleKeys | qbx | cd | f_real | mk | mono | okok | qs | renewed | t1ger | wasabi ]
+BridgeClientConfig.Fuel              = "auto"       -- [ LegacyFuel | ox_fuel | ps-fuel | qs-fuelstations | Renewed-Fuel | ti_fuel | x-fuel | okokGasStation | BigDaddy | cdn | sna |  ] 
+BridgeClientConfig.TargetSystem      = "ox"         -- [ ox | qb | sleepless | auto ] -- sleepless is only partially supported and would need another system in place
 
 return BridgeClientConfig
