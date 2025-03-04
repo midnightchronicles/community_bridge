cd7f41b5180a2dbf1e3a75049146ccee8525e3d6
MrNewb <47620135+MrNewb@users.noreply.github.com>
2025-03-03 19:08:58 -0500

    managment to auto or manual

---
+++ modules/managment/_default/server.lua
@@ -0,0 +1,13 @@
+Managment = Managment or {}
+
+Managment.GetAccountMoney = function(account)
+    return false, print("The resource you are using does not support this function.")
+end
+
+Managment.AddAccountMoney = function(account, amount)
+    return false, print("The resource you are using does not support this function.")
+end
+
+Managment.RemoveAccountMoney = function(account, amount)
+    return false, print("The resource you are using does not support this function.")
+end
