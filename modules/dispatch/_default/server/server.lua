d4f9bf28b46ae2c92bc6b35d93110eed1f461147
No.1 <105018824+harbiyelidev@users.noreply.github.com>
2025-05-22 15:39:37 +0300

    Update server.lua

--- modules/dispatch/_default/server/server.lua
+++ modules/dispatch/_default/server/server.lua
@@ -4,7 +4,7 @@ RegisterNetEvent("community_bridge:Server:DispatchAlert", function(data)
     local jobs = data.jobs
     for _, name in pairs(jobs) do
         local activeJobPlayers = Bridge.Framework.GetPlayersByJob(name)
-        for src, _ in pairs(activeJobPlayers) do
+        for _, src in pairs(activeJobPlayers) do
             TriggerClientEvent('community_bridge:Client:DispatchAlert', src, data)
         end
     end
