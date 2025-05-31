d191782388cae48028dda331e230945a96aaab48
gononono64 <96976330+gononono64@users.noreply.github.com>
2025-05-30 23:37:10 -0500

    Clothing update
    includes illenium and fivem appearance
    
    fivem appearance needs testing

---
+++ modules/clothing/illenium-appearance/client.lua
@@ -0,0 +1,7 @@
+if GetResourceState('illenium-appearance') == 'missing' then return end
+Clothing = Clothing or {}
+
+function Clothing.OpenMenu()
+    TriggerEvent('qb-clothing:client:openMenu')
+end
+
