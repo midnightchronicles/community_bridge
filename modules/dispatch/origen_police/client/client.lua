f8777d3a9049c7c00f76e910cabe850a1cbf801a
Daniel GP <96537843+DaniGP17@users.noreply.github.com>
2025-06-21 15:50:33 +0200

    fix(dispatch): update vehicle color in SendAlert function

--- modules/dispatch/origen_police/client/client.lua
+++ modules/dispatch/origen_police/client/client.lua
@@ -3,6 +3,11 @@ if GetResourceState('origen_police') == 'missing' then return end
 Dispatch = Dispatch or {}
 
 Dispatch.SendAlert = function(data)
+    local color = nil
+    if data.vehicle then
+        local r, g, b = GetVehicleColor(data.vehicle)
+        color = {r, g, b}
+    end
     local customData = {
         coords = data.coords or vector3(0.0, 0.0, 0.0),
         title = 'Alert '..(data.code or '10-80'),
@@ -10,7 +15,7 @@ Dispatch.SendAlert = function(data)
         job = data.jobs or 'police',
         metadata = {
             model = data.vehicle and (GetDisplayNameFromVehicleModel(GetEntityModel(data.vehicle))) or nil,
-            color = {255, 255, 255},
+            color = color,
             plate = data.vehicle and GetVehicleNumberPlateText(data.vehicle) or nil,
             speed = data.vehicle and (GetEntitySpeed(veh) * 3.6)..' kmh' or nil
         }
